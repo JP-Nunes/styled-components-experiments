778be160243a5bcc2e8822b99e05b52c86dea7ca
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-12-23 21:41:35 -0300

    Starting styled grid experiment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+const Container = styled.div`
+  max-width: 1360px;
+  padding: 0 15px;
+  margin: 0 auto;
+  box-sizing: border-box;
+
+  &:before, &:after {
+    content: " ";
+    display: table;
+  }
+
+  &:after {
+    clear: both;
+  }
+`
+
+const Row = styled.div`
+  width: 100%;
+  height: auto;
+  float: left;
+  box-sizing: border-box;
+
+  &:before, &:after {
+    content: " ";
+    display: table;
+  }
+
+  &:after {
+    clear: both;
+  }
+`
+
+const Column = styled.div`
+  float: left;
+  padding: .25rem;
+  min-height: 1px;
+  box-sizing: border-box;
+  width: 100%;
+
+  @media only screen and (min-width: 768px) {
+    width: ${props => (props.grid ? props.grid / 12 * 100 : '8:33')}%
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Column grid='12'>grid 5</Column>
+        <Column grid='5'>grid 5</Column>
+        <Column grid='7'>grid 2</Column>
+      </Row>
+    </Container>
   );
 }
 
